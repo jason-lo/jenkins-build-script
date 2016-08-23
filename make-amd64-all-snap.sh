dbcdbe9d6b1a1bead35df065777df5088811a7db
lo <jason.lo@canonical.com>
2016-08-23 11:09:47 +0800

    first commit

---
+++ make-amd64-all-snap.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+TODAY=$(date +%Y%m%d)
+MINOR_RELEASE=$1
+CHANNEL=$2
+PROJECT=$3
+
+if [ -z $CHANNEL ] ; then
+#	CHANNEL=stable
+	CHANNEL=edge
+fi
+
+if [ -z $PROJECT ] ; then
+	PROJECT=snappy-16
+fi
+
+/jenkins-build-script/ubuntu-device-flash --verbose core 16 \
+	--channel $CHANNEL \
+	--size 4 \
+	--enable-ssh \
+	--gadget pc \
+	--kernel pc-kernel \
+	--os ubuntu-core \
+	--install snapweb \
+	--install network-manager \
+	--install modem-manager \
+	--install bluez \
+	-o $PROJECT-$TODAY-$MINOR_RELEASE.img
